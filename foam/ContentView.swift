cccfce39a3dd79ce996b09defd9174df7bcc0afc
Austin Wood <i@austinwood.me>
2021-02-21 19:53:14 -0700

    Initial Commit

---
+++ foam/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  foam
+//
+//  Created by austin_wood on 2/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ foam/foamApp.swift
@@ -0,0 +1,17 @@
+//
+//  foamApp.swift
+//  foam
+//
+//  Created by austin_wood on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct foamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
